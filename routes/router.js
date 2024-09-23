9d2e56f42cc12a38e34ebfbe2ab79942a2362647
Charlyn Iriejo Cuasay <charlyncuasay94@gmail.com>
2024-09-23 23:59:22 +0800

    gg

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ck = require('../controller/cController.js');
+const ck = require('../controller/cController');
 
 router.get('/', ck.char);
 router.get('/about', ck.lyn);
